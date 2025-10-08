b1097e0fafcbd82fa8473a23e8cb6671efac9ed6
Sergio Zarcero Perez <zar0_31@outlook.com>
2025-10-08 17:31:03 +0200

    feat: agregar producto
    se realiza un metodo para agregar un producto con una validacion de error en la clase inventario

--- inventario-js-poo/src/models/inventario.js
+++ inventario-js-poo/src/models/inventario.js
@@ -2,4 +2,13 @@ class Inventario{
     constructor(){
        this.productos = [];
     }
+
+    agregarProducto(producto){ 
+        if (!(producto instanceof Producto)) { 
+            console.error(' solo se pueden agregar instancias de productos');
+            return;
+        }
+        this.productos.push(producto);
+        console.log(Producto); //se que esto no se hace, es para pruebas
+    }
 }
